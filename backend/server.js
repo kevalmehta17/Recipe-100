4436b856becf73db742dee28f548f3e6dd123c0f
Keval Mehta <kevalmehta885@gmail.com>
2025-10-22 12:21:08 +0530

    feat: updated

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+// middleware
+app.use(express.json());
+
+// routes
+
+app.use("/api/auth", authRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+})
