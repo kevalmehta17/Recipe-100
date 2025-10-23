9d85c5667b9609f63692c42b5196ba09786ec56f
Keval Mehta <kevalmehta885@gmail.com>
2025-10-23 19:54:29 +0530

    feat: auth middleware

---
+++ backend/middleware/auth.middleware.js
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+
+
+export const protectRoute = async (req, res) => {
+    try {
+        // Authorization : Bearer <token>
+        const authHeader = req.header("Authorization");
+
+        if (!authHeader || !authHeader.startsWith("Bearer")) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+        // extract the token (at index 1)
+        const token = authHeader.split(' ')[1];
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        if (!decoded) {
+            return res.status(401).json({
+                success: false,
+                message : "Unauthorized"
+            })
+        }
+
+        // attach the user id (mongo id) 
+        req.user = decoded.id;
+        next();
+        
+    } catch (error) {
+        console.error("Error in ProtectRoute", error.message);
+        return res.status(401).json({
+            message : "Unauthorized"
+        })
+    }
+}
