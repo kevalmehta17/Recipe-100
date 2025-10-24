cba7a248bbb13fb3c4c207f20b757a453911aaf9
Keval Mehta <kevalmehta885@gmail.com>
2025-10-24 20:23:26 +0530

    fix: issue solved

--- backend/model/recipe.model.js
+++ backend/model/recipe.model.js
@@ -4,22 +4,22 @@ const recipeSchema = new mongoose.Schema({
     title: {
         type: String,
         required: [true, "Title required"],
-        maxLength: [100, "Recipe title can not exceed 100 characters"],
-        trim: True
+        maxLength: [100, "Recipe title cannot exceed 100 characters"],
+        trim: true
     },
     description: {
         type: String,
-        required: [true, "Description Required"],
+        required: [true, "Description required"],
         trim: true,
-        maxLength: [1000, "Recipe description cannot exceed 500 characters"]
+        maxLength: [1000, "Recipe description cannot exceed 1000 characters"]
     },
     ingredients: {
-        type: [string], //array of string
+        type: [String],
         required: [true, "Ingredients required"]
     },
     instructions: {
-        type: [string],
-        required: [true, "Instructions are required"],
+        type: [String],
+        required: [true, "Instructions are required"]
     },
     type: {
         type: String,
@@ -37,14 +37,15 @@ const recipeSchema = new mongoose.Schema({
     },
     createdBy: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
+        ref: "User",
+        required: true
     },
-    likes: {
+    likes: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
-    }
+    }]
 }, { timestamps: true });
 
-const Recipe = mongoose.model("Recipe", "recipeSchema");
+const Recipe = mongoose.model("Recipe", recipeSchema);
 
 export default Recipe;
