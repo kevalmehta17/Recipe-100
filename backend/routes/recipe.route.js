60aaac6bc523894fecfe6b75eed556df77a7ddaf
Keval Mehta <kevalmehta885@gmail.com>
2025-10-28 01:57:17 +0530

    feat: recipe.routes.js completed

--- backend/routes/recipe.route.js
+++ backend/routes/recipe.route.js
@@ -1,28 +1,40 @@
 import express from "express";
-import {protectRoute} from "../middleware/auth.middleware.js";
-import { getAllRecipe, getRecipeById } from "../controllers/recipe.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { 
+  createRecipe, 
+  deleteRecipe, 
+  getAllRecipe, 
+  getLikedRecipes, 
+  getMyRecipe, 
+  getRecipeById, 
+  getSavedRecipe, 
+  likeRecipe, 
+  savedRecipe, 
+  updateRecipe 
+} from "../controllers/recipe.controller.js";
 
 const router = express.Router();
 
-// public route
+// ----- PUBLIC ROUTES ----- //
 router.get("/", getAllRecipe);
-router.get("/:id", getRecipeById);
 
-// protected Route
+// ----- PROTECTED ROUTES ------ //
+
+// Create, update, delete
 router.post("/", protectRoute, createRecipe);
 router.patch("/:id", protectRoute, updateRecipe);
 router.delete("/:id", protectRoute, deleteRecipe);
 
-// fetch the list 
+// ---- FETCH USER-SPECIFIC LISTS ---- //
+// IMPORTANT: define these before "/:id" routes
 router.get("/user/me", protectRoute, getMyRecipe);
-router.get("/user/me/likes", protectRoute, getLikedRecipes); 
-router.get("/user/me/saved", protectRoute, getSavedRecipes); 
+router.get("/user/me/likes", protectRoute, getLikedRecipes);
+router.get("/user/me/saved", protectRoute, getSavedRecipe);
 
-// Like or save a specific recipe (action)
+// ---- ACTION ROUTES (like/save) ---- //
 router.post("/:id/like", protectRoute, likeRecipe);
-router.post("/:id/save", protectRoute, saveRecipe);
+router.post("/:id/save", protectRoute, savedRecipe);
 
-
-
+router.get("/:id", getRecipeById);
 
 export default router;
