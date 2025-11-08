8e68fbb82af7ff46ee7e09f3b6a8c9a4d1443b28
Keval Mehta <kevalmehta885@gmail.com>
2025-11-08 22:26:59 +0530

    fix: updated

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/auth.route.js";
 import recipeRoutes from "./routes/recipe.route.js";
@@ -14,6 +15,14 @@ dotenv.config();
 const PORT = process.env.PORT || 4000;
 
 const app = express();
+
+// cors
+app.use(cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
 
 // middleware
 app.use(express.json());
